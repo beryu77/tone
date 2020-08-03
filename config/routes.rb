e34749fda4dee8c9b2facc3f77c27ef189da76ae
watanabe <r.watanabe.coo@gmail.com>
2020-08-03 17:37:41 +0900

    feat: プロフィール画面に投稿画像を追加

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
   resources :users
-  resources :posts, only: [:new, :create, :destroy]
+  resources :posts, only: [:new, :create, :show, :destroy]
   
 end
