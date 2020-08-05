594793f81180671697e12cf821b5511a71aafaa7
watanabe <r.watanabe.coo@gmail.com>
2020-08-05 22:00:17 +0900

    フォロー機能の追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,7 +18,16 @@
 #  index_users_on_email  (email) UNIQUE
 #
 class User < ApplicationRecord
-  has_many :posts, dependent: :destroy   # ユーザーが削除された時に投稿も削除する
+  has_many :posts, dependent: :destroy   
+  has_many :active_relationships, class_name: "Relationship",
+                   foreign_key: "follower_id",
+                   dependent: :destroy
+  has_many :passive_relationships, class_name: "Relationship",
+                   foreign_key: "followed_id",
+                   dependent: :destroy
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
+
   mount_uploader :avatar, AvatarUploader
 
   attr_accessor :remember_token # 仮想の属性（トークンをデータベースに保存せずに実装するため）
@@ -59,4 +68,27 @@ class User < ApplicationRecord
   def forget
     update_attribute(:remember_digest, nil)
   end
+
+
+  # ユーザーをフォローする
+  def follow(other_user)
+    following << other_user
+  end
+
+  # ユーザーをフォロー解除する
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  # 現在のユーザーがフォローしてたらtrueを返す
+  def following?(other_user)
+    following.include?(other_user)
+  end
+
+
+
+
+
+
+
 end
