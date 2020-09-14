155598c22aaa883ba850102fe637177c3908a40a
watanabe <r.watanabe.coo@gmail.com>
2020-09-14 20:48:02 +0900

    feat: コンテスト機能のページ編集

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,3 +1,25 @@
+# == Schema Information
+#
+# Table name: posts
+#
+#  id         :bigint           not null, primary key
+#  caption    :text(65535)
+#  equipment  :string(255)
+#  image      :string(255)
+#  location   :string(255)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  user_id    :bigint
+#
+# Indexes
+#
+#  index_posts_on_user_id                 (user_id)
+#  index_posts_on_user_id_and_created_at  (user_id,created_at)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (user_id => users.id)
+#
 require 'rails_helper'
 
 RSpec.describe 'ポスト', type: :model do
