fa2063e141d48eb0dc12ef28e4788dda67ffd263
watanabe <r.watanabe.coo@gmail.com>
2020-10-01 01:37:21 +0900

    fix: Dockerfile等の修正

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,7 @@ RUN apt-get update && apt-get install -y nodejs --no-install-recommends && rm -r
 RUN apt-get update && apt-get install -y default-mysql-client --no-install-recommends && rm -rf /var/lib/apt/lists/*
 RUN apt-get update -qq && apt-get install -y build-essential libpq-dev nodejs
 
+# Rails App
 RUN mkdir /tone 
 ENV APP_ROOT /tone 
 WORKDIR $APP_ROOT
@@ -13,3 +14,13 @@ ADD ./Gemfile.lock $APP_ROOT/Gemfile.lock
 
 RUN bundle install
 ADD . $APP_ROOT
+
+RUN mkdir -p tmp/sockets
+
+# Expose volumes to frontend
+VOLUME /app/public
+VOLUME /app/tmp
+
+# Start Server
+# TODO: environment
+CMD bundle exec puma
--- config/puma.rb
+++ config/puma.rb
@@ -4,20 +4,20 @@
 # the maximum value specified for Puma. Default is set to 5 threads for minimum
 # and maximum; this matches the default thread size of Active Record.
 #
-threads_count = ENV.fetch('RAILS_MAX_THREADS') { 5 }
+threads_count = ENV.fetch('RAILS_MAX_THREADS') { 5 }.to_i
 threads threads_count, threads_count
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 #
-# port        ENV.fetch('PORT') { 3000 }
-bind "unix://#{Rails.root}/tmp/sockets/puma.sock"
+port        ENV.fetch('PORT') { 3000 }
+# bind "unix://#{Rails.root}/tmp/sockets/puma.sock"
 
 # Specifies the `environment` that Puma will run in.
 #
 environment ENV.fetch('RAILS_ENV') { 'development' }
 
 # Specifies the `pidfile` that Puma will use.
-pidfile ENV.fetch('PIDFILE') { 'tmp/pids/server.pid' }
+# pidfile ENV.fetch('PIDFILE') { 'tmp/pids/server.pid' }
 
 # Specifies the number of `workers` to boot in clustered mode.
 # Workers are forked webserver processes. If using threads and workers together
@@ -36,3 +36,6 @@ pidfile ENV.fetch('PIDFILE') { 'tmp/pids/server.pid' }
 
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
+
+app_root = File.expand_path('..', __dir__)
+bind "unix://#{app_root}/tmp/sockets/puma.sock"
