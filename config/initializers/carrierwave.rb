0397bfa3c3444c4bf08356d1fb1e43b25381f36c
watanabe <r.watanabe.coo@gmail.com>
2020-10-14 18:50:24 +0900

    fix

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,7 +1,7 @@
 unless Rails.env.development? || Rails.env.test?
   CarrierWave.configure do |config|
     config.fog_credentials = {
-      provider: 'AWS',
+      :provider              => 'AWS',
       :region                => ENV['S3_REGION'],   
       :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
       :aws_secret_access_key => ENV['S3_SECRET_KEY']
