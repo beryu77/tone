76075991e625776ab6d71bf954577701f6b904af
watanabe <r.watanabe.coo@gmail.com>
2020-10-15 14:03:40 +0900

    fix アクセスキーの記載方法の修正

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,12 +1,12 @@
 unless Rails.env.development? || Rails.env.test?
   CarrierWave.configure do |config|
     config.fog_credentials = {
-      :provider              => 'AWS',
-      :region                => ENV['S3_REGION'],   
-      :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
-      :aws_secret_access_key => ENV['S3_SECRET_KEY']
+      provider: 'AWS',
+      region: ENV['S3_REGION'],
+      aws_access_key_id: ENV['S3_ACCESS_KEY'],
+      aws_secret_access_key: ENV['S3_SECRET_KEY']
     }
-    config.fog_directory     =  ENV['S3_BUCKET']
+    config.fog_directory = ENV['S3_BUCKET']
     config.cache_storage = :fog
   end
 end
